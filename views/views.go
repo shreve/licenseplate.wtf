9762ae395e30eb8dc91d4e1e121ed1b82e10a0ae
Violet Shreve <shreve@umich.edu>
2023-06-09 15:30:37 -0400

    Add meta descriptions to pages

--- views/views.go
+++ views/views.go
@@ -12,27 +12,6 @@ import (
 	"github.com/kataras/blocks"
 )
 
-type Params map[string]interface{}
-
-type View struct {
-	Title     string
-	Canonical string
-	Template  string
-	Layout    string
-}
-
-func (v View) Render(out io.Writer, params Params) error {
-	if params == nil {
-		params = Params{}
-	}
-	params["Template"] = v.Template
-	params["Layout"] = v.Layout
-	params["Title"] = renderString(v.Title, &params)
-	params["Canonical"] = renderString(v.Canonical, &params)
-	log.Println("Rendering template", v.Template)
-	return views.ExecuteTemplate(out, v.Template, v.Layout, params)
-}
-
 func renderString(in string, params *Params) string {
 	out := &strings.Builder{}
 	tmpl := template.Must(template.New("").Parse(in))
@@ -65,20 +44,46 @@ func init() {
 	log.Println("Loaded views:", names)
 }
 
+type Params map[string]interface{}
+
+type View struct {
+	Title       string
+	Description string
+	Canonical   string
+	Template    string
+	Layout      string
+}
+
+func (v View) Render(out io.Writer, params Params) error {
+	if params == nil {
+		params = Params{}
+	}
+	params["Template"] = v.Template
+	params["Layout"] = v.Layout
+	params["Title"] = renderString(v.Title, &params)
+	params["Description"] = renderString(v.Description, &params)
+	params["Canonical"] = renderString(v.Canonical, &params)
+	log.Println("Rendering template", v.Template)
+	return views.ExecuteTemplate(out, v.Template, v.Layout, params)
+}
+
 var Home = View{
-	Template:  "home",
-	Title:     "WTF is licenseplate.wtf?",
-	Canonical: "https://licenseplate.wtf",
+	Template:    "home",
+	Title:       "WTF is licenseplate.wtf?",
+	Description: "licenseplate.wtf is a place to share your thoughts on custom license plates.",
+	Canonical:   "https://licenseplate.wtf",
 }
 
 var PlateShow = View{
-	Template:  "plates/show",
-	Title:     "What does the license plate [{{.Plate.Code}}] mean?",
-	Canonical: "https://licenseplate.wtf/plates/{{.Plate.Code}}",
+	Template:    "plates/show",
+	Title:       "What does the license plate [{{.Plate.Code}}] mean?",
+	Description: "See what other people think about the license plate [{{.Plate.Code}}].",
+	Canonical:   "https://licenseplate.wtf/plates/{{.Plate.Code}}",
 }
 
 var PlateList = View{
-	Template:  "plates/list",
-	Title:     "WTF do these plates mean?",
-	Canonical: "https://licenseplate.wtf/plates",
+	Template:    "plates/list",
+	Title:       "WTF do these plates mean?",
+	Description: "See what other people think about these license plates.",
+	Canonical:   "https://licenseplate.wtf/plates",
 }
